1f6e3eaac02422e6ee7d0313a3bd77e10680c0cc
Chris Grubb <ctgrubb@vt.edu>
2021-04-28 17:20:22 -0400

    testing whether precompiled vignettes will work properly with pkgdown

---
+++ useful_stuff.R
@@ -0,0 +1,4 @@
+# Build -> Install and Restart does not build vignettes correctly
+# To do this, Build -> More -> Build Source Package, then...
+install.packages("~/Documents/Research/lemur.pack_0.0.0.9000.tar.gz", repos = NULL, type = "source")
+
