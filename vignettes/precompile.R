e43b7810ff59ef3943109f5e55893de0bc89d699
Chris Grubb <ctgrubb@vt.edu>
2021-05-03 21:58:30 -0400

    added precompile.R to repo

---
+++ vignettes/precompile.R
@@ -0,0 +1,9 @@
+# Precompiled vignettes that take too long
+# Must manually move image files from lemur.pack/ to lemur.pack/vignettes/ after knit
+
+library(knitr)
+system("rm -rf figure/")
+knit("vignettes/articles/binary-1d-example.Rmd.orig", "vignettes/articles/binary-1d-example.Rmd")
+system("rm vignettes/articles/figure/*")
+system("mv figure/* vignettes/articles/figure/")
+system("rm -rf figure/")
