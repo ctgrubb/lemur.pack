31b8587610f48ae8ac082e332691fc56dbd0e8b3
Chris Grubb <ctgrubb@vt.edu>
2021-05-19 15:27:42 -0400

    Added univariate continuous intro

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -2,9 +2,12 @@
 # Must manually move image files from lemur.pack/ to lemur.pack/vignettes/ after knit
 
 library(knitr)
-
 system("rm -rf figure/")
+
 knit("vignettes/articles/discreteIntro.Rmd.orig", "vignettes/articles/discreteIntro.Rmd")
-system("rm vignettes/articles/figure/*")
-system("mv figure/* vignettes/articles/figure/")
+system("mv -f figure/* vignettes/articles/figure/")
+system("rm -rf figure/")
+
+knit("vignettes/articles/continuousIntro.Rmd.orig", "vignettes/articles/continuousIntro.Rmd")
+system("mv -f figure/* vignettes/articles/figure/")
 system("rm -rf figure/")
