55c9932e3dddd26e0ee1b28317131a1b185db7c0
Chris Grubb <ctgrubb@vt.edu>
2021-12-09 16:48:51 -0500

    move to laptop

--- chris/ideas.R
+++ chris/ideas.R
@@ -49,3 +49,32 @@ step_1 <- 1 - choose(sum(pop > ord.samp[1]), 10) / choose(100, 10)
 
 # P(X_(2) <= x_(2) | X_(1) <= x_(1))
 step_2 <- 1 - choose(sum(pop > ord.samp[2]), 9) * choose(sum(pop <= ord.samp[1]), 1) / choose(100, 10) / step_1
+
+
+pop <- rnorm(100)
+samp <- sample(pop, 10)
+
+dave.func <- function(pop, samp) {
+  samp <- sort(samp)
+
+  N <- length(pop)
+  n <- length(samp)
+
+  gt.tallies <- sapply(samp, function(x) sum(pop > x))
+  lte.tallies <- N - gt.tallies
+
+  probs <- sapply(1:n, function(x) choose(gt.tallies[x], n - (x - 1)))
+  for(i in 1:(n-1)) {
+    probs <- probs * c(rep(1, i), rep(choose(lte.tallies[i], 1), n - i))
+  }
+  probs <- probs / choose(N, n)
+  probs <- probs / c(1, probs[-n])
+  probs <- 1 - probs
+
+  1 - choose(gt.tallies[1], n) / choose(N, n)
+  1 - choose(gt.tallies[2], n - 1) * choose(lte.tallies[1], 1) / choose(N, n)
+  1 - choose(gt.tallies[3], n - 2) * choose(lte.tallies[2], 1) * choose(lte.tallies[1], 1) / choose(N, n)
+  # ...
+  1 - choose(gt.tallies[10], n - 9)
+}
+
